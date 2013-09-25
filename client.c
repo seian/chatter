2a4e19a85f23aaeaaf3ab14fdc36d330f00c2573
Lim Byunghoon <seian@me.com>
2013-09-26 02:19:50 +0900

    chatter ver.0.1

---
+++ client.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(char *msg);
+void help(char *addr);
+
+int main(int argc, char *argv[]) {
+	if(argc != 4) {
+		help(argv[0]);
+		exit(1);
+	}
+
+	int sockfd;
+	int port;
+	int chk;
+	char buf[256];
+	struct sockaddr_in serverAddr;
+	struct hostent *server;	
+
+	port = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(sockfd < 0)
+		error("ERROR socket open failed");
+	
+	server = gethostbyname(argv[1]);
+
+	if(server == NULL) {
+		fprintf(stderr, "ERROR no host");
+		exit(0);
+	}
+	
+	bzero((char *) &serverAddr, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,
+	      (char *) &serverAddr.sin_addr.s_addr,
+		server->h_length);
+	serverAddr.sin_port = htons(port);
+
+	chk = connect(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	
+	if(chk < 0)
+		error("ERROR connection failed");
+
+	while(1) {
+		bzero(buf, 256);
+		printf("you => ");
+		fgets(buf, 255, stdin);
+		
+		if(!strcmp(buf, "/q"))
+			break;
+
+		chk = write(sockfd, buf, strlen(buf));
+
+		if(chk < 0) 
+			error("ERROR on writing");
+		
+		bzero(buf, 256);
+		chk = read(sockfd, buf, 255);
+
+		if(chk < 0)
+			error("ERROR on reading");
+
+		printf("%s\n", buf);
+	}
+	
+	close(sockfd);
+		
+
+	return 0;
+}
+
+void error(char *msg) {
+	perror(msg);
+	exit(1);
+}
+
+void help(char *addr) {
+	printf("chatter ver.0.1\n");
+	printf("====================================\n");
+	printf("usage : %s [ip] [port] [user name]\n", addr);
+}
---
+++ server.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(char *msg);
+void help(char *addr);
+
+int main(int argc, char *argv[]) {
+	if(argc != 3) {
+		help(argv[0]);
+		exit(1);
+	}
+		
+	int sockfd;
+	int newsockfd;
+	int port;
+	int chk;
+	struct sockaddr_in serverAddr;
+	struct sockaddr_in clientAddr;
+	char buf[256];
+	socklen_t clilen;
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(sockfd < 0)
+		error("ERROR socket open failed");
+
+	port = atoi(argv[1]);
+	bzero((char *) &serverAddr, sizeof(serverAddr));
+	serverAddr.sin_family		= AF_INET;
+	serverAddr.sin_addr.s_addr	= INADDR_ANY;
+	serverAddr.sin_port		= htons(port);
+	chk = bind(sockfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	
+	if(chk < 0)
+		error("ERROR bind failed");
+
+	listen(sockfd, 5);
+	clilen = sizeof(clientAddr);
+	newsockfd = accept(sockfd, (struct sockaddr *) &clientAddr, &clilen);
+	printf("connected!\n");
+
+	if(newsockfd < 0)
+		error("ERROR acception failed");
+
+	while(1) {
+		bzero(buf, 256);
+		chk = read(newsockfd, buf, 255);
+		
+		if(chk < 0)
+			error("ERROR connection failed");
+
+		printf("%s\n", buf);
+		printf("you => ");
+		bzero(buf, 256);
+		fgets(buf, 255, stdin);
+		
+		if(!strcmp(buf, "/q")) 
+			break;
+
+		chk = write(newsockfd, buf, strlen(buf));
+		
+		if(chk < 0)
+			error("ERROR on writing");
+
+	}
+	
+	close(newsockfd);
+	close(sockfd);
+
+
+	return 0;
+}
+
+void error(char *msg) {
+	perror(msg);
+	exit(1);
+}
+
+void help(char *addr) {
+	printf("Chatter V.0.1 BETA\n");
+	printf("============================\n");
+	printf("USAGE: %s <port> <user name>\n", addr);
+}
